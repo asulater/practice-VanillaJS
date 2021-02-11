da0a1f40405fdf4d99d729f0d81c6ba00171494d
asulater <asulater@gmail.com>
2021-02-11 20:01:21 +0900

    2nddayupdates

--- calculator/index.js
+++ calculator/index.js
@@ -2,24 +2,70 @@ const numberInput = document.querySelector('#input');
 const clearBtn = document.querySelector('#clear');
 const plusBtn = document.querySelector('#plus');
 const minusBtn = document.querySelector('#minus');
-const divideBtn = document.querySelector('#multiply');
-const multiBtn = document.querySelector('#divide');
+const divideBtn = document.querySelector('#divide');
+const multiBtn = document.querySelector('#multiply');
 const calBtn = document.querySelector('#calculate');
 const resultInput = document.querySelector('#result');
 
-const temp
-const operator
+let temp;
+let operator;
 
 plusBtn.addEventListener('click', () => {
     if(numberInput.value) {
-        
+        temp = Number(numberInput.value);
+        operator = '+';
+        numberInput.value = null;
+    }
+});
+minusBtn.addEventListener('click', () => {
+    if(numberInput.value) {
+        temp = Number(numberInput.value);
+        operator = '-'
+        numberInput.value = null;
+    }
+});
+multiBtn.addEventListener('click', () => {
+    if(numberInput.value) {
+        temp = Number(numberInput.value);
+        operator = '*';
+        numberInput.value = null;
+    }
+});
+divideBtn.addEventListener('click', () => {
+    if(numberInput.value) {
+        temp = Number(numberInput.value);
+        operator = '/';
+        numberInput.value = null;
     }
 });
 
 clearBtn.addEventListener('click', () => {
-    numberInput.textContent = '';
+    numberInput.value = '';
+    resultInput.value = '';
+    temp = null;
+    operator = null;
 });
 
-resultInput.addEventListener('click', () => {
-
+calBtn.addEventListener('click', () => {
+    if(operator) {
+        if(numberInput.value) {
+            if(operator === '+') {
+                resultInput.value = temp + Number(numberInput.value);
+                numberInput.value = null;
+            } else if (operator === '-') {
+                resultInput.value = temp - Number(numberInput.value);
+                numberInput.value = null;
+            } else if (operator === '*') {
+                resultInput.value = temp * Number(numberInput.value);
+                numberInput.value = null;
+            } else if (operator === '/') {
+                resultInput.value = temp / Number(numberInput.value);
+                numberInput.value = null;
+            }
+        }
+    } else {
+        if(numberInput.value) {
+            resultInput.value = temp;
+        }
+    }
 });
