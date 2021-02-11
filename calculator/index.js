2fbe72b15803d7ccbd6167217b0fa1688c3a2271
asulater <asulater@gmail.com>
2021-02-11 18:56:03 +0900

    2nddayupdates

---
+++ calculator/index.js
@@ -0,0 +1,25 @@
+const numberInput = document.querySelector('#input');
+const clearBtn = document.querySelector('#clear');
+const plusBtn = document.querySelector('#plus');
+const minusBtn = document.querySelector('#minus');
+const divideBtn = document.querySelector('#multiply');
+const multiBtn = document.querySelector('#divide');
+const calBtn = document.querySelector('#calculate');
+const resultInput = document.querySelector('#result');
+
+const temp
+const operator
+
+plusBtn.addEventListener('click', () => {
+    if(numberInput.value) {
+        
+    }
+});
+
+clearBtn.addEventListener('click', () => {
+    numberInput.textContent = '';
+});
+
+resultInput.addEventListener('click', () => {
+
+});
